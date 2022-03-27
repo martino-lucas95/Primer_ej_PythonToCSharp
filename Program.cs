04292535ac2ca722b8a0e26d47e0a8d0435f7f7f
martino-lucas95 <71530057+martino-lucas95@users.noreply.github.com>
2022-03-27 16:59:16 -0300

    New Branch

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace Character
     {
         static void Main(string[] args)
         {
+            //Agrego comentario para firstCommit del branch
             Character p1,p2;
             p1 = new Character("p1",100,70,"ataque1");
             p2 = new Character("p2",100,75,"ataque2");
